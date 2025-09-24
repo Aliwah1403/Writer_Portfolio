05c3ba74e1a0168a2b64c44c824639e4c0dee280
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-09-24 14:49:18 +0400

    feat: Add a recent books/stories section

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import { Hero228 } from "@/components/hero228";
 import { Cta23 } from "@/components/cta23";
 import React from "react";
+import { Gallery23 } from "@/components/gallery23";
 
 const Homepage = () => {
   return (
     <>
       <Hero228 />
+      <Gallery23 />
       <Cta23 />
     </>
   );
