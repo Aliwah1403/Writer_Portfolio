f9c5af7d8ecbe349c7dbdf24eafbc47a80b6b1ed
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-09-24 15:45:32 +0400

    feat: Add art section

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -2,12 +2,19 @@ import { Hero228 } from "@/components/hero228";
 import { Cta23 } from "@/components/cta23";
 import React from "react";
 import { Gallery23 } from "@/components/gallery23";
+import { Gallery30 } from "@/components/gallery30";
 
 const Homepage = () => {
   return (
     <>
       <Hero228 />
+      {/* Recent Books/Stories */}
       <Gallery23 />
+      {/* About Me */}
+      {/* Testimonial from readers */}
+      {/* Art Section */}
+      <Gallery30 />
+      {/* CTA */}
       <Cta23 />
     </>
   );
