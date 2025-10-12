26733e4d59ab52971c69c11f8044255040dd4fb7
Curtis Aliwah <aliwahcurtis5@gmail.com>
2025-10-12 13:19:41 +0400

    chore: Added appID for easier deployment of CMS studio

--- studio-writer_portfolio/sanity.cli.ts
+++ studio-writer_portfolio/sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4bs723xy',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'v0zeztnch13wexpajqr2wz30',
     autoUpdates: true,
-  }
+  },
 })
